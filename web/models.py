8b14babaf28c5bed52f368248ae40bf8322c6c8b
Saeed Hoorsfand <hoorsfand@gmail.com>
2023-01-20 21:23:58 +0330

    add income

--- web/models.py
+++ web/models.py
@@ -6,8 +6,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Expense(models.Model):
-    text = models.CharField(max_length=255)
-    date = models.DateTimeField()
+    text   = models.CharField(max_length=255)
+    date   = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
+    user   = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.text,self.date,self.amount)
     
+class Income(models.Model):
+    text   = models.CharField(max_length=255)
+    date   = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user   = models.ForeignKey(User) 
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.text,self.date,self.amount)
+        
